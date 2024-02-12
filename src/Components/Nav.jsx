fa6f8f5c4dd3b28e19e7b58e465393b9dc9a6fd4
leowader <101485176+leonardo15LD@users.noreply.github.com>
2024-02-11 19:22:26 -0500

    z-50

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,7 +2,7 @@
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 export default function Nav() {
   return (
-    <div className="fixed bottom-0 right-0 flex w-full  justify-center items-center p-2 drop-shadow-2xl">
+    <div className="fixed z-50 bottom-0 right-0 flex w-full  justify-center items-center p-2 drop-shadow-2xl">
       <div className=" flex bg-black p-2 text-white rounded-full gap-2 hover:text-[#FF7A21] cursor-pointer">
         <div className="w-10 h-10 flex justify-center items-center bg-white bg-opacity-10 rounded-full">
           <ShoppingCartIcon fontSize="20px" />
